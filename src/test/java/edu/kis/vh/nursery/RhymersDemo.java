8891f864693ba37f3e677fc6f890327c546914e3
Mikołaj <t255648@edu.p.lodz.pl>
2026-03-16 18:30:57 +0100

    Stale i annotacje

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,29 +5,33 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int ITERATIONS = 15;
+    public static final int TO_FILL = 3;
+    public static final int BOUND = 20;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+
+        for (int i = 1; i < ITERATIONS; i++)
+            for (int j = 0; j < TO_FILL; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
-        
+        for (int i = 1; i < ITERATIONS; i++)
+            rhymers[3].countIn(rn.nextInt(BOUND));
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
+
     }
-    
+
 }
